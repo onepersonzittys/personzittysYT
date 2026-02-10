32fb1430ea42f4ebc13bf7490f8437cb6250d785
aiden <259509332+onepersonzittys@users.noreply.github.com>
2026-02-09 23:56:16 -0300

    Implement noclip toggle GUI in noclip.lua
    Adds a GUI toggle button for noclip functionality.

---
+++ testscrpt/noclip.lua
@@ -0,0 +1,41 @@
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+--variavel de controle
+local onep = Players.LocalPlayer
+local noclip = false
+
+--cria uma base ao menu do jogador.
+local gui = Instance.new("Screengui")
+gui.Name = "testfloder 3"
+gui.Parent = onep:WaitForChild("PlayerGui")
+
+-- cria um button
+local button = Instance.new ("textbutton")
+button.Size = UDim2.new(0, 250, 0, 50)
+button.Position = UDim2.new(0.5, -100, 0.5, -25)
+button.Text = "noclip toggle"
+button.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+button.Parent = gui
+
+--cria um toggle
+button.MouseButton1Click:Connect(function()
+	noclip = not noclip
+
+	if noclip then
+		button.Text = "noclip ON"
+	else
+		button.Text = "noclip OFF"
+	end
+end)
+
+--ativa o noclip atraves do clique
+RunService.Stepped:Connect(function()
+	print("notify player noclip toggle")
+	if noclip and onep.Character then
+		for _, v in pairs(onep.Character:GetDescendants()) do
+			if v:IsA("BasePart") and v.CanCollide == true then
+				v.CanCollide = false
+			end
+		end
+	end
+end)
