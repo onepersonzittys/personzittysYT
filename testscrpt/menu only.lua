46b0760bfbc0ec64d671741c18d7f8b5817348d6
aiden <259509332+onepersonzittys@users.noreply.github.com>
2026-02-13 17:12:41 -0300

    Fix typo in ScreenInsets property

--- testscrpt/menu only.lua
+++ testscrpt/menu only.lua
@@ -6,7 +6,7 @@ local player = game.Players.LocalPlayer
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Name = "ScreenGui"
 ScreenGui.ResetOnSpawn = false
-ScreenGui.ScreenInsets = Enum.ScreenInsets.CoreUiSafeInsects
+ScreenGui.ScreenInsets = Enum.ScreenInsets.CoreUiSafeInsets
 ScreenGui.Parent = player:WaitForChild("PlayerGui")
 
 local MenuFrame = Instance.new("MenuFrame")
