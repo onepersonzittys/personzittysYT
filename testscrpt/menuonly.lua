aca4004718d5521d98bba0c89986e9afbe5f5a4f
aiden <259509332+onepersonzittys@users.noreply.github.com>
2026-02-16 13:54:37 -0300

    Update menuonly.lua

--- testscrpt/menuonly.lua
+++ testscrpt/menuonly.lua
@@ -23,7 +23,7 @@ local MenuTop = Instance.new("ImageLabel")
 MenuTop.Name = "MenuTop"
 MenuTop.Size = UDim2.new(0, 384, 0, 32)
 MenuTop.Position = UDim2.new(0, 0, 0, 0)
-MenuTop.Image = "rbxassetid://119324806847241"
+MenuTop.Image = "rbxthumb://type=Asset&id=119324806847241&w=768&h=32"
 MenuTop.ScaleType = Enum.ScaleType.Slice
 MenuTop.SliceCenter = Rect.new(32, 0, 352, 32)
 MenuTop.ZIndex = 6
