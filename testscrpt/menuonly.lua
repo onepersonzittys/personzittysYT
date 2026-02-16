62cce2fc24bc003ec88ad2eb907d98bf3149c714
aiden <259509332+onepersonzittys@users.noreply.github.com>
2026-02-16 13:28:01 -0300

    Adjust MenuTop size and position in menuonly.lua

--- testscrpt/menuonly.lua
+++ testscrpt/menuonly.lua
@@ -22,8 +22,8 @@ MenuFrame.Parent = ScreenGui
 
 local MenuTop = Instance.new("ImageLabel")
 MenuTop.Name = "MenuTop"
-MenuTop.Size = UDim2.new(0, 0, 0, 0)
-MenuTop.Position = UDim2.new(0, 0, 0, 352)
+MenuTop.Size = UDim2.new(0, 384, 0, 32)
+MenuTop.Position = UDim2.new(0, 0, 0, 0)
 MenuTop.Image = "rbxassetid://119324806847241"
 MenuTop.ScaleType = Enum.ScaleType.Slice
 MenuTop.SliceCenter = Rect.new(32, 32, 352, 32)
