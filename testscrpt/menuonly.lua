74b9f95462d9181c1e660cc7b47a2dfcdd1718cd
aiden <259509332+onepersonzittys@users.noreply.github.com>
2026-02-20 22:06:34 -0300

    Update menuonly.lua

--- testscrpt/menuonly.lua
+++ testscrpt/menuonly.lua
@@ -22,8 +22,8 @@ MenuFrame.Parent = ScreenGui
 
 local MenuTop = Instance.new("ImageLabel")
 MenuTop.Name = "MenuTop"
-MenuTop.Position = UDim2.new(0.5, 0, 0, 5)
-MenuTop.Size = UDim2.new(0, 384, 0, 16)
+MenuTop.Position = UDim2.new(0, -64, 0, 0)
+MenuTop.Size = UDim2.new(0, 448, 0, 16)
 MenuTop.Image = "rbxassetid://117813558054556"
 MenuTop.AnchorPoint = Vector2.new(0.5, 0.5)
 MenuTop.BackgroundTransparency = 1
@@ -43,6 +43,91 @@ resizeHandle.BackgroundTransparency = 1
 resizeHandle.ZIndex = 6
 resizeHandle.Active = true
 resizeHandle.Parent = MenuFrame
+
+local MenuBar = Instance.new("Frame")
+MenuBar.Name = "MenuBar"
+MenuBar.Position = UDim2.new(0, -64, 0, 0)
+MenuBar.Size = UDim2.new(0, 64, 0.9, 0)
+MenuBar.BackgroundColor3 = Color3.fromRGB(82,82,82)
+MenuBar.ZIndex = 4
+MenuBar.Active = true
+MenuBar.Parent = MenuFrame
+local Corner = Instance.new("UICorner")
+Corner.CornerRadius = UDim.new(0, 32)
+Corner.Parent = MenuBar
+
+local BarScrolling = Instance.new("ScrollingFrame")
+BarScrolling.Name = "BarScrolling"
+BarScrolling.Position = UDim2.new(0, 12, 0, 32)
+BarScrolling.Size = UDim2.new(1, -6, 1, -6)
+BarScrolling.BackgroundTransparency = 1
+BarScrolling.BorderSizePixel = 0
+BarScrolling.VerticalScrollBarPosition = Enum.VerticalScrollBarPosition.Left
+BarScrolling.CanvasSize = UDim2.new(0, 0, 0, 0)
+BarScrolling.ScrollBarThickness = 15
+BarScrolling.ScrollingEnabled = true
+BarScrolling.ClipsDescendants = true
+BarScrolling.ZIndex = 5
+BarScrolling.Active = true
+BarScrolling.Parent = MenuBar
+
+local SiblingListing = Instance.new("UIListLayout")
+SiblingListing.Name = "SiblingListing"
+SiblingListing.FillDirection = Enum.FillDirection.Vertical
+SiblingListing.VerticalAlignment = Enum.VerticalAlignment.Top
+SiblingListing.SortOrder = Enum.SortOrder.Name
+SiblingListing.HorizontalFlex = Enum.UIFlexAlignment.Fill
+SiblingListing.ItemLineAlignment = Enum.ItemLineAlignment.Start
+SiblingListing.Padding = UDim.new(0, 4)
+SiblingListing.Parent = BarScrolling
+SiblingListing:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
+	BarScrolling.CanvasSize = UDim2.new(0,0,0,SiblingListing.AbsoluteContentSize.Y)
+end)
+
+local MainButton = Instance.new("TextButton")
+MainButton.Name = "A1MainButton"
+MainButton.Size = UDim2.new(1, -4, 0, 32)
+MainButton.Text = "Main"
+MainButton.Font = Enum.Font.Code
+MainButton.BackgroundColor3 = Color3.fromRGB(232,233,234)
+MainButton.ZIndex = 6
+MainButton.Active = true
+MainButton.Parent = BarScrolling
+
+local lowCorner = Instance.new("UICorner")
+lowCorner.CornerRadius = UDim.new(0, 8)
+lowCorner.Parent = MainButton
+
+MainButton.MouseEnter:Connect(function()
+	MainButton.BackgroundColor3 = Color3.fromRGB(250, 250, 250)
+end)
+
+MainButton.MouseLeave:Connect(function()
+	MainButton.BackgroundColor3 = Color3.fromRGB(232,233,234)
+end)
+
+local CreditsButton = Instance.new("TextButton")
+CreditsButton.Name = "ZZ1000CreditsButton"
+CreditsButton.Size = UDim2.new(1, -4, 0, 32)
+CreditsButton.Text = "Credits"
+CreditsButton.Font = Enum.Font.Code
+CreditsButton.BackgroundColor3 = Color3.fromRGB(232,233,234)
+CreditsButton.ZIndex = 6
+CreditsButton.Active = true
+CreditsButton.Parent = BarScrolling
+
+local lowCorner2 = Instance.new("UICorner")
+lowCorner2.CornerRadius = UDim.new(0, 8)
+lowCorner2.Parent = CreditsButton
+
+CreditsButton.MouseEnter:Connect(function()
+	CreditsButton.BackgroundColor3 = Color3.fromRGB(250, 250, 250)
+end)
+
+CreditsButton.MouseLeave:Connect(function()
+	CreditsButton.BackgroundColor3 = Color3.fromRGB(232,233,234)
+end)
+
 --smooth drag code
 
 local dragging = false
@@ -121,6 +206,8 @@ end)
 UserInputService.InputChanged:Connect(function(input)
 	if not resizing then return end
 	if input ~= sizeInput then return end
+	local frameSize = MenuFrame.AbsoluteSize
+	local frameSizeX = frameSize.X
 	local mousePos = input.Position
 	local deltaSizeX = (mousePos.X - resizeStart.X)
 	local deltaSizeY = (mousePos.Y - resizeStart.Y)
@@ -134,8 +221,8 @@ UserInputService.InputChanged:Connect(function(input)
 	end
 	MenuFrame.Size = UDim2.new(startSize.X.Scale, targetWidth, 
 		startSize.Y.Scale, targetHeight)
-	MenuTop.Size = UDim2.new(startSize.X.Scale, targetWidth,
-		startSize.Y.Scale, 16)
+	MenuTop.Size = UDim2.new(0, targetWidth, 0, 16)
+	MenuBar.Size = UDim2.new(0, frameSizeX / 6 , 0.9, 0)
 end)
 
 UserInputService.InputEnded:Connect(function(input)
